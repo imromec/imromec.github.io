707e631544dea33c9541334958a718c05b45054f
Meghraj Suthar <meghrajsuthar03@gmail.com>
2018-03-08 16:23:15 +0530

    Update task.js file

---
+++ task.js
@@ -0,0 +1,36 @@
+if (!input) {
+    output = "[]";
+} else {
+    var stringLen;
+    stringLen = input.length;
+
+    if(stringLen > 1){
+    output = '[';
+    var stringArray = input.split("");
+    var totalCharas = stringArray.length;
+
+    for(var i = 0; i < totalCharas; i++){
+      if(stringArray[i] == '(')
+        output += (output != "[") ? ' [' : "";
+      else if(stringArray[i] == " ")
+        if(stringArray[i-1] == "(" || stringArray[i+1] == ")")
+           continue;
+       else
+        output += ", ";
+      else if(stringArray[i] == ')')
+        output += ']';
+      else
+        output += '"'+stringArray[i]+'"';
+    }
+
+    var outputArray = output.split("");
+    var lenOutput = outputArray.length;
+
+    output += (outputArray[lenOutput-1] != ']') ? ']' : "";
+    output += (output.split("[").length != output.split("]").length) ? "]" : "";
+  }
+  else{
+    $output = '["'+input+'"]';
+  }
+
+}
