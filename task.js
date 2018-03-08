69d79050cf55f34646c3e41c7b3f27994c06008a
Meghraj Suthar <meghrajsuthar03@gmail.com>
2018-03-08 16:24:18 +0530

    Updated task.js file

--- task.js
+++ task.js
@@ -1,36 +1,42 @@
-if (!input) {
-    output = "[]";
-} else {
-    var stringLen;
-    stringLen = input.length;
+function transform(input) {
+    var output;
+    output = "";
+    // TODO: Write your code here
+    if (!input) {
+        output = "[]";
+    } else {
+        var stringLen;
+        stringLen = input.length;
 
-    if(stringLen > 1){
-    output = '[';
-    var stringArray = input.split("");
-    var totalCharas = stringArray.length;
+        if(stringLen > 1){
+            output = '[';
+            var stringArray = input.split("");
+            var totalCharas = stringArray.length;
 
-    for(var i = 0; i < totalCharas; i++){
-      if(stringArray[i] == '(')
-        output += (output != "[") ? ' [' : "";
-      else if(stringArray[i] == " ")
-        if(stringArray[i-1] == "(" || stringArray[i+1] == ")")
-           continue;
-       else
-        output += ", ";
-      else if(stringArray[i] == ')')
-        output += ']';
-      else
-        output += '"'+stringArray[i]+'"';
+            for(var i = 0; i < totalCharas; i++){
+                if(stringArray[i] == '(')
+                    output += (output != "[") ? ' [' : "";
+                else if(stringArray[i] == " ")
+                    if(stringArray[i-1] == "(" || stringArray[i+1] == ")")
+                       continue;
+                   else
+                    output += ", ";
+                else if(stringArray[i] == ')')
+                    output += ']';
+                else
+                    output += '"'+stringArray[i]+'"';
+            }
+
+            var outputArray = output.split("");
+            var lenOutput = outputArray.length;
+
+            output += (outputArray[lenOutput-1] != ']') ? ']' : "";
+            output += (output.split("[").length != output.split("]").length) ? "]" : "";
+        }
+        else{
+            $output = '["'+input+'"]';
+        }
+
     }
-
-    var outputArray = output.split("");
-    var lenOutput = outputArray.length;
-
-    output += (outputArray[lenOutput-1] != ']') ? ']' : "";
-    output += (output.split("[").length != output.split("]").length) ? "]" : "";
-  }
-  else{
-    $output = '["'+input+'"]';
-  }
-
+    return output;
 }
